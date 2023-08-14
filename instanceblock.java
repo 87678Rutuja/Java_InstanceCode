30b088ef3abca709f67f8ee1baf5bbef353212e6
87678Rutuja <132226247+87678Rutuja@users.noreply.github.com>
2023-08-14 20:57:46 +0530

    Add files via upload

---
+++ instanceblock.java
@@ -0,0 +1,13 @@
+class Demo{
+	int x = 10;
+	Demo(){
+		System.out.println("constractor");
+	}
+}
+	System.out.println("Instance Block");
+}
+	public static void main(String[] ar);
+		Demo obj = new Demo();
+			System.out.println("main");
+}
+}
